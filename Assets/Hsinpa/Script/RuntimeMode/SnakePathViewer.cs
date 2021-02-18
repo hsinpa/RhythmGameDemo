dc56742a4786ce1bff0420789a70d9afdd853ab9
hsinpa <hsinpa@gmail.com>
2021-02-18 17:36:19 +0800

    Add very little script on snake component spawning

--- Assets/Hsinpa/Script/RuntimeMode/SnakePathViewer.cs
+++ Assets/Hsinpa/Script/RuntimeMode/SnakePathViewer.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Hsinpa.Snake;
+using Utility;
 
 namespace Hsinpa {
     public class SnakePathViewer : MonoBehaviour
@@ -12,21 +14,98 @@ namespace Hsinpa {
         [SerializeField]
         private LevelSRP LevelSRP;
 
+        [SerializeField]
+        private Transform SnakeHolder;
+
+        [SerializeField]
+        private SnakeMesh snakePrefab;
+
         private Types.LevelJSON levelJSON;
         private float startTime;
         private float currentTime;
 
+        private int noteIndex;
+        private int noteLength;
+
+        private const float defaultNoteTime = 1;
+        private float noteTime = defaultNoteTime;
+
+        private Dictionary<string, System.Action<Types.LevelComponent>> NoteActionTable = new Dictionary<string, System.Action<Types.LevelComponent>>();
+
         public void Start()
         {
+            NoteActionTable = RegisterNoteTable();
+            noteIndex = -1;
+            
+            PlaySnakeView();
+        }
+
+        private Dictionary<string, System.Action<Types.LevelComponent>> RegisterNoteTable() {
+            Dictionary<string, System.Action<Types.LevelComponent>> actionTable = new Dictionary<string, System.Action<Types.LevelComponent>>();
+
+            actionTable.Add(EventFlag.LevelComponent.SnakeType, ProcessSnakeNote);
+            actionTable.Add(EventFlag.LevelComponent.SpeedType, ProcessSpeedNote);
+
+            return actionTable;
+        }
+
+        private void PlaySnakeView()
+        {
+            noteIndex = 0;
             startTime = Time.time;
             levelJSON = JsonUtility.FromJson<Types.LevelJSON>(LevelJsonData.text);
+            noteLength = levelJSON.sequence.Length;
+
+            UtilityMethod.ClearChildObject(SnakeHolder);
         }
 
         private void Update()
         {
+            //Not start yet
+            if (noteIndex < 0) return;
+
             currentTime = Time.time - startTime;
+
+            bool doneProcessing = false;
+
+            while (!doneProcessing) {
+
+                bool noteAvailable = isNextNoteAvailable(noteIndex);
+
+                if (noteAvailable) {
+                    ProcessNote(noteIndex);
+                    noteIndex++;
+                }
+
+                doneProcessing = !noteAvailable;
+            }
 
             Debug.Log(currentTime);
         }
+
+        private void ProcessNote(int index) {
+
+            Types.LevelComponent levelComp = levelJSON.sequence[index];
+
+            if (NoteActionTable.TryGetValue(levelComp.type, out System.Action<Types.LevelComponent> action)) {
+                action(levelComp);
+            }
+
+        }
+
+        private void ProcessSpeedNote(Types.LevelComponent component) {
+            Debug.Log($"Type {component.type}, Time {component.time}, Value {component.value}");
+        }
+
+        private void ProcessSnakeNote(Types.LevelComponent component) {
+            Debug.Log($"Type {component.type}, Time {component.time}, Value {component.value}");
+        }
+
+        private bool isNextNoteAvailable(int index) {
+            if (index >= noteLength) return false;
+
+            return (levelJSON.sequence[index].time < (currentTime + noteTime));
+        }
+
     }
 }
---
+++ Assets/Hsinpa/Script/Utility/EventFlag.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventFlag
+{
+    public class LevelComponent {
+        public const string SnakeType = "snake";
+        public const string SpeedType = "speed";
+    }
+
+}
