4db61604e0e58e06ca3e9785035f4866c5096768
hsinpa <hsinpa@gmail.com>
2021-02-22 11:55:37 +0800

    Add input manager (no logic yet)

---
+++ Assets/Hsinpa/Script/RuntimeMode/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hsinpa.InputSystem {
+    public class InputManager : MonoBehaviour
+    {
+
+        Camera _camera;
+
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+
+            if (Input.GetMouseButtonDown(0)) {
+                OnScreenTouch(Input.mousePosition);
+            }
+
+        }
+
+        private void OnScreenTouch(Vector2 screenPos) {
+            Ray ray = _camera.ScreenPointToRay(screenPos);
+
+            Debug.Log($"Screen {screenPos}, Origin {ray.origin}, Direction {ray.direction}");
+        }
+
+    }
+}
